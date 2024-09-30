c02856f43772793763ae7827e807ccb3a053e8c1
hadas <hadasmeitav@gmail.com>
2024-09-30 13:23:29 +0300

    fix: not being able to load client html

--- server/app.js
+++ server/app.js
@@ -12,6 +12,7 @@ const options = {
     cert: fs.readFileSync(path.join(__dirname, 'server.cert'))
 };
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(cors());
 app.use(routesAuthentication);
